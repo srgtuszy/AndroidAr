4358c8a3c0c9d1c1e0e16c8dd6022dfeb6d9773c
Michał Tuszyński <srgtuszy@gmail.com>
2013-01-04 01:55:48 +0100

    Changed method name fromLocation to fromTitle

--- src/com/srgtuszy/arlibrary/model/ArItemFactory.java
+++ src/com/srgtuszy/arlibrary/model/ArItemFactory.java
@@ -4,7 +4,7 @@ import android.location.Location;
 
 public class ArItemFactory {
 
-    public static ArItem fromLocation(String title, Location location) {
+    public static ArItem fromTitle(String title, Location location) {
         ArItemImpl item = new ArItemImpl();
         item.setItemLocation(location);
         item.setTitle(title);
